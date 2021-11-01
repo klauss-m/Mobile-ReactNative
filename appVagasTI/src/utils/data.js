e31a6e33949618e3105c09ffaa35f4eb58863084
Klauss Marques <klauss.marques@gmail.com>
2021-11-01 02:51:47 -0300

    feat: add mock data

---
+++ appVagasTI/src/utils/data.js
@@ -0,0 +1,53 @@
+const vagas = [
+  {
+    id: 1,
+    titulo: 'Desenvolvedor Front-End',
+    descricao: 'Vaga para desenvolvedor front-end',
+    salario: 'R$ 3.000,00',
+    cidade: 'São Paulo',
+  },
+  {
+    id: 2,
+    titulo: 'Desenvolvedor Back-End',
+    descricao: 'Vaga para desenvolvedor back-end',
+    salario: 'R$ 3.000,00',
+    cidade: 'São Paulo',
+  },
+  {
+    id: 3,
+    titulo: 'Desenvolvedor Full-Stack',
+    descricao: 'Vaga para desenvolvedor full-stack',
+    salario: 'R$ 3.000,00',
+    cidade: 'São Paulo',
+  },
+  {
+    id: 4,
+    titulo: 'Desenvolvedor Mobile',
+    descricao: 'Vaga para desenvolvedor mobile',
+    salario: 'R$ 3.000,00',
+    cidade: 'São Paulo',
+  },
+  {
+    id: 5,
+    titulo: 'Desenvolvedor Web',
+    descricao: 'Vaga para desenvolvedor web',
+    salario: 'R$ 3.000,00',
+    cidade: 'São Paulo',
+  },
+  {
+    id: 6,
+    titulo: 'Project Manager',
+    descricao: 'Vaga para project manager',
+    salario: 'R$ 7.000,00',
+    cidade: 'São Paulo',
+  },
+  {
+    id: 7,
+    titulo: 'Analista de Sistemas',
+    descricao: 'Vaga para analista de sistemas',
+    salario: 'R$ 4.500,00',
+    cidade: 'São Paulo',
+  },
+];
+
+export default vagas;
