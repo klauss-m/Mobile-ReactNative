ca6525f46146a7862d76e1c77b371f118d16bcea
Klauss Marques <klauss.marques@gmail.com>
2021-11-03 02:28:20 -0300

    chore: first commit

---
+++ appMeuPerfilTabNav/src/pages/formacao/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function Formacao() {
+  return (
+    <View>
+      <Text>Formação</Text>
+    </View>
+  );
+}
