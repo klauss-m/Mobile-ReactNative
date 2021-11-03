2e5edf3c7ad00ff88e4532132816062a78303796
Klauss Marques <klauss.marques@gmail.com>
2021-11-03 00:32:15 -0300

    chore: first commit

---
+++ appMeuPerfilDrawer/src/App.js
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
